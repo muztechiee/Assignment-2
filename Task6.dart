f87f4608448a3945456f6cb8738a2e09e5782852
muztechiee <muztechiee@gmail.com>
2025-10-25 22:12:10 +0500

    Task 6

---
+++ Task6.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age1 = 25;
+  int age2 = 30;
+
+  if (age1 > age2) {
+    print("Person 1 is older, Person 2 is younger.");
+  } else if (age1 < age2) {
+    print("Person 2 is older, Person 1 is younger.");
+  } else {
+    print("Both are of the same age.");
+  }
+}
