58c50dc33b2dfe4e7f85bf94fe5567bc47c442e4
muztechiee <muztechiee@gmail.com>
2025-10-25 22:07:45 +0500

    Task 4

---
+++ Task4.dart
@@ -0,0 +1,17 @@
+void main() {
+  int marks = 82;
+
+  if (marks >= 90) {
+    print("Grade: A+");
+  } else if (marks >= 80) {
+    print("Grade: A");
+  } else if (marks >= 70) {
+    print("Grade: B");
+  } else if (marks >= 60) {
+    print("Grade: C");
+  } else if (marks >= 50) {
+    print("Grade: D");
+  } else {
+    print("Grade: F (Fail)");
+  }
+}
