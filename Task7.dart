6598e2a9440bac5181b96069a686c69cf6166e08
muztechiee <muztechiee@gmail.com>
2025-10-25 22:14:18 +0500

    Task 7

---
+++ Task7.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = -10;
+
+  if (num > 0) {
+    print("Number is positive");
+  } else if (num < 0) {
+    print("Number is negative");
+  } else {
+    print("Number is zero");
+  }
+}
